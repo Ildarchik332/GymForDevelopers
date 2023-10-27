ab235c10542f9dd1d50a8afeea9de28f59dc907b
Ildarchik332 <140722709+Ildarchik332@users.noreply.github.com>
2023-10-28 01:04:32 +0300

    Создал сущности Вопрос и ответ.

--- src/main/java/com/dev/GymForDevelopers/controllers/GdAnswerController.java
+++ src/main/java/com/dev/GymForDevelopers/controllers/GdAnswerController.java
@@ -1,30 +1,28 @@
 package com.dev.GymForDevelopers.controllers;
 
-import com.dev.GymForDevelopers.converters.GdAnswerConverter;
+import com.dev.GymForDevelopers.converters.GdConvertAnswer;
 import com.dev.GymForDevelopers.models.DTO.GdAnswerDTO;
+import com.dev.GymForDevelopers.models.entity.GdAnswer;
 import com.dev.GymForDevelopers.services.GdAnswerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/answer")
 public class GdAnswerController {
     private final GdAnswerService gdAnswerService;
-    private final GdAnswerConverter gdAnswerConverter;
+    private final GdConvertAnswer gdConvertAnswer;
 
     @Autowired
-    public GdAnswerController(GdAnswerService gdAnswerService, GdAnswerConverter gdAnswerConverter) {
+    public GdAnswerController(GdAnswerService gdAnswerService, GdConvertAnswer gdConvertAnswer) {
         this.gdAnswerService = gdAnswerService;
-        this.gdAnswerConverter = gdAnswerConverter;
+        this.gdConvertAnswer = gdConvertAnswer;
     }
 
     @PostMapping("/create")
     public ResponseEntity<String> create(@RequestBody GdAnswerDTO answerDTO) {
-        gdAnswerService.save(answerDTO.getQuestionId(), gdAnswerConverter.convertToEntity(answerDTO));
+        gdAnswerService.save(gdConvertAnswer.convertToEntity(answerDTO));
         return ResponseEntity.ok("Ваш ответ успешно добавлен");
     }
 }
