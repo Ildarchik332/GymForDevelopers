71cafd31a806e8f692fdccb4c177a3fe63b6116a
Ildarchik332 <140722709+Ildarchik332@users.noreply.github.com>
2023-12-02 17:27:20 +0300

    mail для статуса note

---
+++ src/main/java/com/dev/GymForDevelopers/config/MailConfig.java
@@ -0,0 +1,44 @@
+package com.dev.GymForDevelopers.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+    @Value("${Gd.mail.host}")
+    private String host;
+
+    @Value("${Gd.mail.port}")
+    private Integer port;
+
+    @Value("${Gd.mail.username}")
+    private String username;
+
+    @Value("${Gd.mail.password}")
+    private String password;
+
+    @Value("${Gd.mail.protocol}")
+    private String protocol;
+
+    @Bean
+    public JavaMailSender getMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(host);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+        mailSender.setPort(port);
+
+        Properties properties = mailSender.getJavaMailProperties();
+        properties.setProperty("mail.transport.protocol", protocol);
+
+        return mailSender;
+    }
+}
+
