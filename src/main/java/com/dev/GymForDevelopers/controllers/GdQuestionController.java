9155c5fd3a2f3383d31c50b7cd7bc23d2ea80897
Ildarchik332 <140722709+Ildarchik332@users.noreply.github.com>
2023-12-22 02:16:36 +0300

    Сделан метод для получения случайных вопросов + правки

--- src/main/java/com/dev/GymForDevelopers/controllers/GdQuestionController.java
+++ src/main/java/com/dev/GymForDevelopers/controllers/GdQuestionController.java
@@ -3,15 +3,15 @@ package com.dev.GymForDevelopers.controllers;
 import com.dev.GymForDevelopers.config.SwaggerConfig;
 import com.dev.GymForDevelopers.converters.GdQuestionConverter;
 import com.dev.GymForDevelopers.models.DTO.GdQuestionDTO;
+import com.dev.GymForDevelopers.models.entity.GdQuestion;
 import com.dev.GymForDevelopers.services.GdQuestionService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Api(tags = SwaggerConfig.QUESTION_TAG)
 @RestController
@@ -33,4 +33,16 @@ public class GdQuestionController {
         return ResponseEntity.ok("Вопрос успешно создан");
     }
 
+    @ApiOperation("Метод для получения случайных вопросов")
+    @GetMapping("/random/{count}")
+    public List<String> getRandom(@PathVariable("count") Integer count) {
+        return gdQuestionService.getRandomQuestion(count);
+    }
+
+    @ApiOperation("Метод для получения случейных вопрос из конкретного раздела")
+    @GetMapping("/random/{section}/{count}")
+    public List<String> getRandomQuestionBySection(@PathVariable("section") String section,
+                                                   @PathVariable("count") Integer count) {
+        return gdQuestionService.getRandomBySection(section, count);
+    }
 }
--- src/main/java/com/dev/GymForDevelopers/services/GdQuestionService.java
+++ src/main/java/com/dev/GymForDevelopers/services/GdQuestionService.java
@@ -1,11 +1,17 @@
 package com.dev.GymForDevelopers.services;
 
+import com.dev.GymForDevelopers.enums.QuestionSections;
 import com.dev.GymForDevelopers.exceptions.ExceptionConst;
 import com.dev.GymForDevelopers.exceptions.GdRuntimeException;
 import com.dev.GymForDevelopers.models.entity.GdQuestion;
 import com.dev.GymForDevelopers.repositories.GdQuestionRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Сервис для работы с вопросами
@@ -13,12 +19,13 @@ import org.springframework.stereotype.Service;
  * @author Ildar
  */
 @Service
+@Slf4j
 public class GdQuestionService {
-    private final GdQuestionRepository questionRepository;
+    private final GdQuestionRepository gdQuestionRepository;
 
     @Autowired
-    public GdQuestionService(GdQuestionRepository questionRepository) {
-        this.questionRepository = questionRepository;
+    public GdQuestionService(GdQuestionRepository gdQuestionRepository) {
+        this.gdQuestionRepository = gdQuestionRepository;
     }
 
     /**
@@ -30,6 +37,50 @@ public class GdQuestionService {
         if (gdQuestion == null) {
             throw new GdRuntimeException(ExceptionConst.MESSAGE_RT, ExceptionConst.ERRORS_CODE_RT);
         }
-        questionRepository.save(gdQuestion);
+        gdQuestionRepository.save(gdQuestion);
+    }
+
+    /**
+     * Метод для получения случайных вопросов
+     *
+     * @param count Количество вопросов
+     */
+    public List<String> getRandomQuestion(Integer count) {
+        if (count == null) {
+            log.info("Вы не указали колличество вопросов. По-умолчанию выдано 10");
+            count = 10;
+        }
+        return gdQuestionRepository.getRandomQuestion(count);
+    }
+
+    /**
+     * Метод для получения случайных вопросов из конкретного раздела
+     *
+     * @param section Раздел вопросов
+     * @param count   Количество вопросов
+     */
+    public List<String> getRandomBySection(String section, Integer count) {
+        if (count == null) {
+            log.info("Вы не указали колличество вопросов. По-умолчанию выдано 10");
+            count = 10;
+        }
+        if (section == null) {
+            log.info("Не указан раздел. Вопросы выдаются из случайных разделов");
+            return gdQuestionRepository.getRandomQuestion(count);
+        }
+        boolean sectionExist = false;
+        QuestionSections[] sectionsList = QuestionSections.values();
+        for (QuestionSections x : sectionsList) {
+            if (section.equals(x.getSection())) {
+                sectionExist = true;
+                break;
+            }
+        }
+        if (!sectionExist) {
+            log.error("Ошибка. Введенный раздел не был найден");
+            throw new GdRuntimeException(ExceptionConst.MESSAGE_NF, ExceptionConst.ERRORS_CODE_NF);
+        }
+        return gdQuestionRepository.getRandomFromSection(section, count);
     }
 }
+
